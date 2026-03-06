f166111fb15d907ddf0c6f1d5818ec839ee76c64
snehguptafr <guptasneh302004@gmail.com>
2026-03-06 12:28:46 +0530

    add socket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,33 @@
 import express from "express"
 import cors from "cors"
+import { createServer } from "node:http"
+import { Server } from "socket.io"
 import visitorsRouter from "./routes/visitors"
 import visitsRouter from "./routes/visits"
+import usersRouter from "./routes/users"
 
 const app = express()
+const httpServer = createServer(app)
+
+export const io = new Server(httpServer,  {
+  cors: {origin: "*", methods: ["GET", "POST"]}
+})
+
 app.use(cors())
 app.use(express.json())
+
+io.on("connection", socket => {
+  console.log("Client connected")
+
+  socket.on("join", (userId: string) => {
+    socket.join(userId)
+    console.log(`user ${userId} joined their room`)
+  })
+
+  socket.on("disconnect", ()=>{
+    console.log("client disconnected:", socket.id)
+  })
+})
 
 
 app.get("/health", (req, res) => {
@@ -14,5 +36,6 @@ app.get("/health", (req, res) => {
 
 app.use("/api/visitors", visitorsRouter);
 app.use("/api/visits", visitsRouter)
+app.use("/api/users", usersRouter)
 
-app.listen(4000, () => console.log("Backend running on port 4000"))
+httpServer.listen(4000, () => console.log("Backend running on port 4000"))
