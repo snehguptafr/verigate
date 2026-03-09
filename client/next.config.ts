75d3f00c4f0f51397c74943f7e04a32a6d99bc11
snehguptafr <guptasneh302004@gmail.com>
2026-03-09 16:59:35 +0530

    allow dev origins

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['local-origin.dev', '192.168.175.133']
+  allowedDevOrigins: ['local-origin.dev', '192.168.*.*']
 };
 
 export default nextConfig;
