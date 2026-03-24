f93e99dee0e4fcf713703daefbaa45f0f5a5d3a1
snehguptafr <guptasneh302004@gmail.com>
2026-03-24 14:15:20 +0530

    enable https server

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,17 @@ import visitorsRouter from "./routes/visitors"
 import visitsRouter from "./routes/visits"
 import usersRouter from "./routes/users"
 import authRouter from "./routes/auth"
+import fs from "fs"
+import https from "https"
+import path from "node:path"
 
 const app = express()
-const httpServer = createServer(app)
+const httpsServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, "localhost+1-key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "localhost+1.pem"))
+},app)
 
-export const io = new Server(httpServer, {
+export const io = new Server(httpsServer, {
   cors: { origin: "*", methods: ["GET", "POST"] }
 })
 
@@ -45,4 +51,4 @@ app.use("/api/visits", visitsRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/auth", authRouter)
 
-httpServer.listen(4000, () => console.log("Backend running on port 4000"))
+httpsServer.listen(4000, () => console.log("Backend running on port 4000"))
